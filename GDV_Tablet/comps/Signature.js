5bc4db8b285f4594ec9bbbe8cb202d6a236b95cc
unknown <QuocMinh-PC@CTY9-CNTT-MINH.mobifone.vn>
2017-11-29 17:32:09 +0700

    Fix lỗi PropTypes does not exist

--- GDV_Tablet/comps/Signature.js
+++ GDV_Tablet/comps/Signature.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
 
 // Utils
 import { Utils, Params } from "../utils/Utils.js";
+// Library to draw
+import SketchView from 'react-native-sketch-view';
+
+const sketchViewConstants = SketchView.constants;
+
+const tools = {};
+
+tools[sketchViewConstants.toolType.pen.id] = {
+  id: sketchViewConstants.toolType.pen.id,
+  name: sketchViewConstants.toolType.pen.name,
+  nextId: sketchViewConstants.toolType.eraser.id
+};
+tools[sketchViewConstants.toolType.eraser.id] = {
+  id: sketchViewConstants.toolType.eraser.id,
+  name: sketchViewConstants.toolType.eraser.name,
+  nextId: sketchViewConstants.toolType.pen.id
+};
 
 class Signature extends Component {
-  save = () => {
-    this.sketch.save().then(({ path }) => {
-      Alert.alert('Image saved!', path);
-    });
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      toolSelected: sketchViewConstants.toolType.pen.id
+    };
+  }
+
+  isEraserToolSelected() {
+    console.log('isEraserToolSelected');
+    return this.state.toolSelected === sketchViewConstants.toolType.eraser.id;
+  }
+
+  toolChangeClick() {
+    this.setState({ toolSelected: tools[this.state.toolSelected].nextId });
+  }
+
+  getToolName() {
+    return tools[this.state.toolSelected].name;
+  }
+
+  onSketchSave(saveEvent) {
+    this.props.onSave && this.props.onSave(saveEvent);
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
+        <SketchView style={{ flex: 1, backgroundColor: 'white' }} ref="sketchRef"
+          selectedTool={this.state.toolSelected}
+          onSaveSketch={this.onSketchSave.bind(this)}
+          localSourceImagePath={this.props.localSourceImagePath} />
+
+        <View style={{ flexDirection: 'row', backgroundColor: '#EEE' }}>
+          <TouchableHighlight underlayColor={"#CCC"} style={{ flex: 1, alignItems: 'center', paddingVertical: 20 }} onPress={() => { this.refs.sketchRef.clearSketch() }}>
+            <Text style={{ color: '#888', fontWeight: '600' }}>CLEAR</Text>
+          </TouchableHighlight>
+          <TouchableHighlight underlayColor={"#CCC"} style={{ flex: 1, alignItems: 'center', paddingVertical: 20, borderLeftWidth: 1, borderRightWidth: 1, borderColor: '#DDD' }} onPress={() => { this.refs.sketchRef.saveSketch() }}>
+            <Text style={{ color: '#888', fontWeight: '600' }}>SAVE</Text>
+          </TouchableHighlight>
+          <TouchableHighlight underlayColor={"#CCC"} style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.isEraserToolSelected() ? "#CCC" : "rgba(0,0,0,0)" }} onPress={this.toolChangeClick.bind(this)}>
+            <Text style={{ color: '#888', fontWeight: '600' }}>ERASER</Text>
+          </TouchableHighlight>
         </View>
       </View>
     );
@@ -27,7 +76,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#2c3e50',
-    width: Params.SCREEN_WIDTH
+    width: Params.SCREEN_WIDTH,
+    height: 200
   },
 });
 
